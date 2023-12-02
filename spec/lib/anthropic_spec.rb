82983f4e00334c8e435b7b1f6ee65c973c4ab653
Dick Davis <dick@hey.com>
2023-12-01 20:34:28 -0600

    Allow api_key to be set

--- spec/lib/anthropic_spec.rb
+++ spec/lib/anthropic_spec.rb
@@ -4,4 +4,44 @@ RSpec.describe Anthropic do
   it 'has a version number' do
     expect(Anthropic::VERSION).not_to be_nil
   end
+
+  describe '.setup' do
+    subject(:setup) do
+      described_class.setup do |anthropic|
+        anthropic.api_key = api_key
+      end
+    end
+
+    let(:api_key) { 'foobar' }
+
+    it 'sets the provided values' do
+      setup
+      expect(described_class.api_key).to eq(api_key)
+    end
+  end
+
+  describe '.api_key' do
+    subject(:call_method) { described_class.api_key }
+
+    let(:api_key) { 'foobar' }
+
+    before do
+      described_class.api_key = api_key
+    end
+
+    it 'returns the API key' do
+      expect(call_method).to eq(api_key)
+    end
+  end
+
+  describe '.api_key=' do
+    subject(:call_method) { described_class.api_key = api_key }
+
+    let(:api_key) { 'foobar' }
+
+    it 'returns the API key' do
+      call_method
+      expect(described_class.api_key).to eq(api_key)
+    end
+  end
 end
